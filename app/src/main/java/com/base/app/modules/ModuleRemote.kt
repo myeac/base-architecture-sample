3308b9996a19a90141aab97ebec0671352fd0206
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-07-07 16:04:49 -0300

    implemented remote module

---
+++ app/src/main/java/com/base/app/modules/ModuleRemote.kt
@@ -0,0 +1,27 @@
+package com.base.app.modules
+
+import com.base.app.data_remote.core.BaseNetworkFactory
+import com.base.app.data_remote.service.FilmApiService
+import com.movies.app.BuildConfig
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+
+private val baseUrl = BuildConfig.BUILD_BASE_URL
+private val json = Json { ignoreUnknownKeys = true }
+
+val remoteModule = module {
+    single {
+        Retrofit
+            .Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .addCallAdapterFactory(BaseNetworkFactory())
+            .build()
+    }
+
+    /** modules */
+    single { get<Retrofit>().create(FilmApiService::class.java) }
+}
