8a075d608e95cd5f66710292cfa717d7c3e5ba49
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-07-08 11:08:48 -0300

    implemented domain error manager

---
+++ app/src/main/java/com/base/app/domain/core/DomainException.kt
@@ -0,0 +1,19 @@
+package com.base.app.domain.core
+
+import org.koin.core.logger.MESSAGE
+
+sealed class DomainException(
+    message: String? = null
+) : Exception(message) {
+    class NetworkErrorException : DomainException(DOMAIN_ERROR_NETWORK)
+    class AccessDeniedException : DomainException(DOMAIN_ERROR_ACCESS_DENIED)
+    class NotFoundException : DomainException(DOMAIN_ERROR_NOT_FOUND)
+    class DatabaseException : DomainException(DOMAIN_ERROR_DATA_BASE)
+    data class UnknownException(val msg: String) : DomainException(msg)
+}
+
+const val DOMAIN_ERROR_GENERIC = "An unexpected error occurred"
+const val DOMAIN_ERROR_NETWORK = "Network error occurred"
+const val DOMAIN_ERROR_ACCESS_DENIED = "Access denied"
+const val DOMAIN_ERROR_NOT_FOUND = "Resource not found"
+const val DOMAIN_ERROR_DATA_BASE = "Database error occurred"
