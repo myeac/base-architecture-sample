05272df1b3e887b3f4115115bcdde0e3c06b6f36
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-07-08 13:57:50 -0300

    implemented use case module

---
+++ app/src/main/java/com/base/app/modules/ModuleUseCase.kt
@@ -0,0 +1,22 @@
+package com.base.app.modules
+
+import com.base.app.domain.use_case.GetFilmByIdUseCase
+import com.base.app.domain.use_case.GetFilmByTitleUseCase
+import com.base.app.domain.use_case.GetListFavoriteFilmsUseCase
+import com.base.app.domain.use_case.RemoveFavoriteFilmUseCase
+import com.base.app.domain.use_case.SaveFavoriteFilmUseCase
+import com.base.app.domain.use_case.SearchFilmInFavoritesByName
+import com.base.app.domain.use_case.GetFilmListBySort
+import com.base.app.domain.use_case.IsFilmFavorite
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory { GetFilmByTitleUseCase(get()) }
+    factory { GetFilmByIdUseCase(get()) }
+    factory { SaveFavoriteFilmUseCase(get()) }
+    factory { RemoveFavoriteFilmUseCase(get()) }
+    factory { GetListFavoriteFilmsUseCase(get()) }
+    factory { SearchFilmInFavoritesByName(get()) }
+    factory { GetFilmListBySort(get()) }
+    factory { IsFilmFavorite(get()) }
+}
