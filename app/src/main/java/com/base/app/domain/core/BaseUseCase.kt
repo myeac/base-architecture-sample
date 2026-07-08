e6192114e8f17ced692cb81af6c886eeb77d5db6
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-07-08 11:08:39 -0300

    implemented base use case

---
+++ app/src/main/java/com/base/app/domain/core/BaseUseCase.kt
@@ -0,0 +1,31 @@
+package com.base.app.domain.core
+
+/**
+ * For UseCases that require parameters.
+ */
+abstract class BaseUseCase<in Params, out DOMAIN> {
+    abstract suspend fun execute(params: Params): DOMAIN
+
+    suspend operator fun invoke(params: Params): DOMAIN {
+        return try {
+            execute(params)
+        } catch (e: Throwable) {
+            throw e.toDomainError()
+        }
+    }
+}
+
+/**
+ * For UseCases that DO NOT require parameters.
+ */
+abstract class BaseNoParamsUseCase<out DOMAIN> {
+    abstract suspend fun execute(): DOMAIN
+
+    suspend operator fun invoke(): DOMAIN {
+        return try {
+            execute()
+        } catch (e: Throwable) {
+            throw e.toDomainError()
+        }
+    }
+}
