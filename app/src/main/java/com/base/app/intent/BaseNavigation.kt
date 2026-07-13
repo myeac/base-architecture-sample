b7b6ecef988e39ab9f526cc78ab86936f97082df
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-07-13 16:42:27 -0300

    implemented base navigation

---
+++ app/src/main/java/com/base/app/intent/BaseNavigation.kt
@@ -0,0 +1,55 @@
+package com.base.app.intent
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.base.app.presentation.FilmDetailScreen
+import com.base.app.presentation.home.FilmSearchTypeUiModel
+import com.base.app.presentation.home.HomeScreen
+
+@Composable
+fun AppNavigation(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeDestination
+    ) {
+        /** HomeScreen */
+        composable<HomeDestination> {
+            val navigator = remember(navController) {
+                object : HomeNavigator {
+                    override fun navigateToDetail(
+                        query: String,
+                        searchType: FilmSearchTypeUiModel
+                    ) {
+                        navController.navigate(FilmDetailDestination(query, searchType))
+                    }
+                    override fun navigateToFavorites() {
+                        navController.navigate(FilmFavoritesDestination)
+                    }
+                }
+            }
+            HomeScreen(navigator = navigator)
+        }
+
+        /** FilmDetailScreen */
+        composable<FilmDetailDestination> {
+            val args = it.toRoute<FilmDetailDestination>()
+            FilmDetailScreen(
+                parameter = args.parameter,
+                searchType = args.searchType
+            )
+        }
+
+        /** FilmFavoritesScreen */
+        composable<FilmFavoritesDestination> {
+            Text(text = "Film Favorites Screen")
+        }
+    }
+}
