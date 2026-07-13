c6a8396dae3213bfe231675084acb90aae76a8a2
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-07-13 16:44:04 -0300

    removed import

--- app/src/main/java/com/base/app/presentation/film_detail/components/FilmHeaderOverlay.kt
+++ app/src/main/java/com/base/app/presentation/film_detail/components/FilmHeaderOverlay.kt
@@ -1,13 +1,11 @@
 package com.base.app.presentation.film_detail.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
