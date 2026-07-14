ed05b35d5f1da6ef1783620fbfea8ffe027aed5a
Miguel Yengle <miguel.acuna@treinus.com.br>
2026-07-14 15:19:47 -0300

    implemented screen

---
+++ app/src/main/java/com/base/app/presentation/film_favorites/FilmFavoriteListScreen.kt
@@ -0,0 +1,23 @@
+package com.base.app.presentation.film_favorites
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.base.app.presentation.film_favorites.view_model.FavoriteFilmsViewModel
+import com.base.app.presentation.home.FilmSearchTypeUiModel
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun FilmFavoriteListScreen(
+    onNavigateToDetail: (String, FilmSearchTypeUiModel) -> Unit,
+    onBack: () -> Unit,
+    viewModel: FavoriteFilmsViewModel = koinViewModel()
+) {
+    val favoriteState by viewModel.favoriteFilmsState.collectAsStateWithLifecycle()
+
+    FilmFavoriteListContent(
+        uiState = favoriteState,
+        onFilmClick = { onNavigateToDetail(it, FilmSearchTypeUiModel.SEARCH_FROM_FAVORITES) },
+        onBack = onBack,
+    )
+}
